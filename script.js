45fa3576211ec7378e4096e57540ad2ecb4c582b
Maksym <makkie.makowski@gmail.com>
2023-10-14 13:52:18 -0400

    add input elements

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
-const url = "https://api.weatherapi.com/v1/current.json?key=98efa64a40434a5a9a8170303230710&q=Zaporizhzhya"
+const url = 'https://api.weatherapi.com/v1/current.json?key=98efa64a40434a5a9a8170303230710&q=';
+let locationVal = 'Saint Petersburg Fl';
+
+const info = document.querySelector('.weather-data>p');
+const input = document.querySelector('input#location');
+const submitBtn = document.querySelector('input#submit');
+
+submitBtn.addEventListener('click', e => {
+    locationVal = input.value;
+    getWeather();
+});
+
+input.addEventListener('keypress', e => {
+    if(e.key === 'Enter') {
+        submitBtn.click();
+    }
+});
+
 
 async function getWeather() {
     try{
-        const response = await fetch(url);
+        const response = await fetch(url+locationVal);
         const data = await response.json();
         if (response.status === 200) {
             console.log(data);
+            info.innerHTML = data.location.name + ' ' + data.current.condition.text;
+            
         } else {
-            console.log('server error ', data.error.message);
+            console.log('server error: ', data.error.message);
         }
     } catch (error) {
-        console.log(error);
+        console.log('error: ', error);
     }
 }
 
